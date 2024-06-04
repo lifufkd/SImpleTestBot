4bc1645df99295c7c9cbd99ea1caf41684e75591
lilfuz <lilfuz@gmail.com>
2024-06-04 23:20:52 +0300

    Finished project

--- backend.py
+++ backend.py
@@ -17,19 +17,30 @@ class TempUserData:
 
     def temp_data(self, user_id):
         if user_id not in self.__user_data.keys():
-            self.__user_data.update({user_id: [None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None]}) #3/0 - никнейм, 4 - капча, 5 - адрес емэйл, 6 - код отправленный на емэйл, 7 - bs64 аватарка, 8 - пароль, 9 - день рождения, 10 - город проживания
+            self.__user_data.update({user_id: [None, None, None, None, None, None, None, None, None, None, None, None, None, None, None]}) #3/0 - никнейм, 4 - капча, 5 - адрес емэйл, 6 - код отправленный на емэйл, 7 - bs64 аватарка, 8 - пароль, 9 - день рождения, 10 - город проживания
         return self.__user_data
 
 
 class AmoCrmLead(_Lead):
-    ffff = custom_field.TextCustomField("ffff")
-    dddddd = custom_field.TextCustomField("dddddd")
+    tg_id = custom_field.NumericCustomField("tg_id")
+    telegram_contact = custom_field.TextCustomField("telegram_contact")
+    user_fio = custom_field.DateCustomField("user_fio")
+    user_birthdate = custom_field.TextCustomField("user_birthdate")
+    user_email = custom_field.TextCustomField("user_email")
+    user_city = custom_field.TextCustomField("user_city")
+    user_nick = custom_field.TextCustomField("user_nick")
+    user_password = custom_field.TextCustomField("user_password")
+    user_photo = custom_field.TextCustomField("user_photo")
+    user_employment = custom_field.TextCustomField("user_employment")
+    user_category = custom_field.TextCustomField("user_category")
+    user_social_problem = custom_field.TextCustomField("user_social_problem")
+    user_environmental_problem = custom_field.TextCustomField("user_environmental_problem")
 
 
 class DbAct:
-    def __init__(self, db, config):
+    def __init__(self, config):
         super(DbAct, self).__init__()
-        self.__db = db
+        # self.__db = db
         self.__config = config
         self.init()
 
@@ -42,6 +53,14 @@ class DbAct:
             storage=tokens.FileTokensStorage(directory_path='./amocrm_tokens'),  # by default FileTokensStorage
         )
 
+    def user_is_existed(self, user_id):
+        temp = list()
+        users = AmoCrmLead.objects.all()
+        for i in users:
+            temp.append(i.tg_id)
+        if user_id in temp:
+            return True
+
     def send_email(self):
         pass
 
@@ -49,47 +68,58 @@ class DbAct:
         tokens.default_token_manager.init(code=self.__config.get_config()['amocrm_temp_code'], skip_error=True)
         sys.exit("Token's succsessfully generated")
 
-    # def add_user(self):
-    #     lead = AmoCrmLead.objects.create()
-    #     lead.name = 'fwefwefweffwe'
-    #     lead.ffff = '123'
-    #     lead.dddddd = '321'
-    #     lead.save()
+    def add_user(self, tg_id, tg_contact, user_photo, data):
+        lead = AmoCrmLead.objects.create()
+        lead.name = data[3]
+        lead.tg_id = tg_id
+        lead.telegram_contact = tg_contact
+        lead.user_fio = data[3]
+        lead.user_email = data[6]
+        lead.user_birthdate = data[4]
+        lead.user_city = data[5]
+        lead.user_nick = data[0]
+        lead.user_password = data[1]
+        lead.user_photo = user_photo
+        lead.user_employment = data[7]
+        lead.user_category = data[8]
+        lead.user_social_problem = data[9]
+        lead.user_environmental_problem = data[10]
+        lead.save()
 
-    def add_user(self, user_id, first_name, last_name, nick_name):
-        if not self.user_is_existed(user_id):
-            if user_id in self.__config.get_config()['admins']:
-                is_admin = True
-            else:
-                is_admin = False
-            self.__db.db_write('INSERT INTO users (user_id, first_name, last_name, nick_name, is_admin) VALUES (?, ?, ?, ?, ?)', (user_id, first_name, last_name, nick_name, is_admin))
-
-    def user_is_existed(self, user_id):
-        data = self.__db.db_read('SELECT count(*) FROM users WHERE user_id = ?', (user_id, ))
-        if len(data) > 0:
-            if data[0][0] > 0:
-                status = True
-            else:
-                status = False
-            return status
-
-    def user_is_admin(self, user_id):
-        data = self.__db.db_read('SELECT is_admin FROM users WHERE user_id = ?', (user_id, ))
-        if len(data) > 0:
-            if data[0][0] == 1:
-                status = True
-            else:
-                status = False
-            return status
-
-    def check_statistic_existed(self, user_id):
-        data = self.__db.db_read('SELECT COUNT(*) FROM statistic WHERE user_id = ?', (user_id, ))
-        if len(data[0]) > 0:
-            if data[0][0] > 0:
-                return True
-
-    def write_statistic(self, balls, user_id):
-        if self.check_statistic_existed(user_id):
-            self.__db.db_write('UPDATE statistic SET balls = ? WHERE user_id = ?', (balls, user_id))
-        else:
-            self.__db.db_write('INSERT INTO statistic (user_id, balls) VALUES(?, ?)', (balls, user_id))
+    # def add_user(self, user_id, first_name, last_name, nick_name):
+    #     if not self.user_is_existed(user_id):
+    #         if user_id in self.__config.get_config()['admins']:
+    #             is_admin = True
+    #         else:
+    #             is_admin = False
+    #         self.__db.db_write('INSERT INTO users (user_id, first_name, last_name, nick_name, is_admin) VALUES (?, ?, ?, ?, ?)', (user_id, first_name, last_name, nick_name, is_admin))
+    #
+    # def user_is_existed(self, user_id):
+    #     data = self.__db.db_read('SELECT count(*) FROM users WHERE user_id = ?', (user_id, ))
+    #     if len(data) > 0:
+    #         if data[0][0] > 0:
+    #             status = True
+    #         else:
+    #             status = False
+    #         return status
+    #
+    # def user_is_admin(self, user_id):
+    #     data = self.__db.db_read('SELECT is_admin FROM users WHERE user_id = ?', (user_id, ))
+    #     if len(data) > 0:
+    #         if data[0][0] == 1:
+    #             status = True
+    #         else:
+    #             status = False
+    #         return status
+    #
+    # def check_statistic_existed(self, user_id):
+    #     data = self.__db.db_read('SELECT COUNT(*) FROM statistic WHERE user_id = ?', (user_id, ))
+    #     if len(data[0]) > 0:
+    #         if data[0][0] > 0:
+    #             return True
+    #
+    # def write_statistic(self, balls, user_id):
+    #     if self.check_statistic_existed(user_id):
+    #         self.__db.db_write('UPDATE statistic SET balls = ? WHERE user_id = ?', (balls, user_id))
+    #     else:
+    #         self.__db.db_write('INSERT INTO statistic (user_id, balls) VALUES(?, ?)', (balls, user_id))
--- main.py
+++ main.py
@@ -7,9 +7,8 @@ import smtplib
 import base64
 import platform
 import telebot
-import copy
+import requests
 import random
-from amocrm.v2 import tokens
 from datetime import datetime
 from email.mime.text import MIMEText
 from captcha.image import ImageCaptcha
@@ -23,6 +22,24 @@ from frontend import Bot_inline_btns
 #####################################
 config_name = 'secrets.json'
 #####################################
+
+
+def upload_image_to_imgur(image_base64, client_id):
+    url = "https://api.imgur.com/3/upload"
+    headers = {
+        "Authorization": f"Client-ID {client_id}"
+    }
+    data = {
+        "image": base64.b64decode(image_base64),
+        "type": "file"
+    }
+    response = requests.post(url, headers=headers, files=data)
+
+    if response.status_code == 200:
+        response_data = response.json()
+        return response_data["data"]["link"]
+    else:
+        raise Exception(f"Failed to upload image: {response.status_code} {response.text}")
 
 
 def generate_captcha(user_id):
@@ -68,128 +85,133 @@ def main():
     def start_message(message):
         command = message.text.replace('/', '')
         user_id = message.chat.id
-        db_actions.add_user(user_id, message.from_user.first_name, message.from_user.last_name,
-                            f'@{message.from_user.username}')
         buttons = Bot_inline_btns()
-        if command == 'start':
+        if db_actions.user_is_existed(user_id):
+            if command == 'start':
+                bot.send_message(user_id, 'Приветствую! Вы уже зарегистрированы',
+                                 reply_markup=buttons.reg_btns())
+                # bot.send_message(user_id, 'Пройти тест', reply_markup=buttons.start_buttons(), parse_mode='html')
+        else:
             bot.send_message(user_id, 'Для начала Вам необходимо зарегестрироваться в боте!⬇️⬇️⬇️',
                              reply_markup=buttons.reg_btns())
-            # bot.send_message(user_id, 'Пройти тест', reply_markup=buttons.start_buttons(), parse_mode='html')
 
     @bot.message_handler(content_types=['text', 'photo'])
     def messages(message):
         user_id = message.chat.id
+        user_nickname = message.from_user.username
         buttons = Bot_inline_btns()
         user_input = message.text
         user_photo = message.photo
         code = temp_user_data.temp_data(user_id)[user_id][0]
-        if db_actions.user_is_existed(user_id):
-            match code:
-                case 0:
-                    if user_input is not None:
-                        temp_user_data.temp_data(user_id)[user_id][4] = user_input
-                        bot.send_message(user_id, 'Придумайте пароль')
-                        temp_user_data.temp_data(user_id)[user_id][0] = 1
+        match code:
+            case 0:
+                if user_input is not None:
+                    temp_user_data.temp_data(user_id)[user_id][4] = user_input
+                    bot.send_message(user_id, 'Придумайте пароль')
+                    temp_user_data.temp_data(user_id)[user_id][0] = 1
+                else:
+                    bot.send_message(user_id, 'Это не текст! Попробуйте ещё раз')
+            case 1:
+                if user_input is not None:
+                    temp_user_data.temp_data(user_id)[user_id][5] = user_input
+                    bot.send_message(user_id, 'Выберите аватарку')
+                    temp_user_data.temp_data(user_id)[user_id][0] = 2
+                else:
+                    bot.send_message(user_id, 'Это не текст! Попробуйте ещё раз')
+            case 2:
+                if user_photo is not None:
+                    photo_id = user_photo[-1].file_id
+                    photo_file = bot.get_file(photo_id)
+                    photo_to_bs64 = base64.b64encode(bot.download_file(photo_file.file_path)).decode('latin1')
+                    temp_user_data.temp_data(user_id)[user_id][6] = photo_to_bs64
+                    bot_captcha(user_id)
+                else:
+                    bot.send_message(user_id, 'Это не фотография! Попробуйте ещё раз')
+            case 3:
+                if user_input == temp_user_data.temp_data(user_id)[user_id][1]:
+                    bot.send_message(user_id, 'Введите ФИО')
+                    temp_user_data.temp_data(user_id)[user_id][0] = 4
+                else:
+                    bot.send_message(user_id, 'Капча не подтверждена')
+                    bot_captcha(user_id)
+            case 4:
+                if user_input is not None:
+                    temp_user_data.temp_data(user_id)[user_id][7] = user_input
+                    bot.send_message(user_id, 'Введите дату рождения (в формате дд.мм.гггг (12.12.2012))')
+                    temp_user_data.temp_data(user_id)[user_id][0] = 5
+                else:
+                    bot.send_message(user_id, 'Это не текст! Попробуйте ещё раз')
+            case 5:
+                if user_input is not None:
+                    try:
+                        temp_user_data.temp_data(user_id)[user_id][8] = datetime.strptime(user_input, '%d.%m.%Y').date()
+                        temp_user_data.temp_data(user_id)[user_id][0] = 6
+                        bot.send_message(user_id, 'Введите город проживания')
+                    except:
+                        bot.send_message(user_id, 'Дата указана в неправильном формате! Попробуйте ещё раз')
+                else:
+                    bot.send_message(user_id, 'Это не текст! Попробуйте ещё раз')
+            case 6:
+                if user_input is not None:
+                    temp_user_data.temp_data(user_id)[user_id][9] = user_input
+                    temp_user_data.temp_data(user_id)[user_id][0] = 7
+                    bot.send_message(user_id, 'Введите E-mail')
+                else:
+                    bot.send_message(user_id, 'Это не текст! Попробуйте ещё раз')
+            case 7:
+                if user_input is not None:
+                    temp_user_data.temp_data(user_id)[user_id][10] = user_input
+                    if send_email(user_id):
+                        temp_user_data.temp_data(user_id)[user_id][0] = 8
+                        bot.send_message(user_id, 'Введите код подтверждения с вашей почты')
                     else:
-                        bot.send_message(user_id, 'Это не текст! Попробуйте ещё раз')
-                case 1:
-                    if user_input is not None:
-                        temp_user_data.temp_data(user_id)[user_id][5] = user_input
-                        bot.send_message(user_id, 'Выберите аватарку')
-                        temp_user_data.temp_data(user_id)[user_id][0] = 2
-                    else:
-                        bot.send_message(user_id, 'Это не текст! Попробуйте ещё раз')
-                case 2:
-                    if user_photo is not None:
-                        photo_id = user_photo[-1].file_id
-                        photo_file = bot.get_file(photo_id)
-                        photo_to_bs64 = base64.b64encode(bot.download_file(photo_file.file_path)).decode('latin1')
-                        temp_user_data.temp_data(user_id)[user_id][6] = photo_to_bs64
-                        bot_captcha(user_id)
-                    else:
-                        bot.send_message(user_id, 'Это не фотография! Попробуйте ещё раз')
-                case 3:
-                    if user_input == temp_user_data.temp_data(user_id)[user_id][1]:
-                        bot.send_message(user_id, 'Введите ФИО')
-                        temp_user_data.temp_data(user_id)[user_id][0] = 4
-                    else:
-                        bot.send_message(user_id, 'Капча не подтверждена')
-                        bot_captcha(user_id)
-                case 4:
-                    if user_input is not None:
-                        temp_user_data.temp_data(user_id)[user_id][7] = user_input
-                        bot.send_message(user_id, 'Введите дату рождения (в формате дд.мм.гггг (12.12.2012))')
-                        temp_user_data.temp_data(user_id)[user_id][0] = 5
-                    else:
-                        bot.send_message(user_id, 'Это не текст! Попробуйте ещё раз')
-                case 5:
-                    if user_input is not None:
-                        try:
-                            temp_user_data.temp_data(user_id)[user_id][8] = datetime.strptime(user_input, '%d.%m.%Y')
-                            temp_user_data.temp_data(user_id)[user_id][0] = 6
-                            bot.send_message(user_id, 'Введите город проживания')
-                        except:
-                            bot.send_message(user_id, 'Дата указана в неправильном формате! Попробуйте ещё раз')
-                    else:
-                        bot.send_message(user_id, 'Это не текст! Попробуйте ещё раз')
-                case 6:
-                    if user_input is not None:
-                        temp_user_data.temp_data(user_id)[user_id][9] = user_input
-                        temp_user_data.temp_data(user_id)[user_id][0] = 7
-                        bot.send_message(user_id, 'Введите E-mail')
-                    else:
-                        bot.send_message(user_id, 'Это не текст! Попробуйте ещё раз')
-                case 7:
-                    if user_input is not None:
-                        temp_user_data.temp_data(user_id)[user_id][10] = user_input
-                        if send_email(user_id):
-                            temp_user_data.temp_data(user_id)[user_id][0] = 8
-                            bot.send_message(user_id, 'Введите код подтверждения с вашей почты')
-                        else:
-                            bot.send_message(user_id, 'Произошла ошибка. Введите адрес почты ещё раз')
-                    else:
-                        bot.send_message(user_id, 'Это не текст! Попробуйте ещё раз')
-                case 8:
-                    if user_input == temp_user_data.temp_data(user_id)[user_id][2]:
-                        bot.send_message(user_id, 'Вы успешно зарегестировались!')
-                        bot.send_message(user_id, 'Укажите вашу занятость', reply_markup=buttons.question_btns())
-                    else:
-                        bot.send_message(user_id, 'Код не верный, попробуйте еще раз!\n\n'
-                                                  'Вы хотите поменять адрес почты?', reply_markup=buttons.email_btns())
-                case 9:
-                    if user_input is not None:
-                        temp_user_data.temp_data(user_id)[user_id][17] = user_input
-                        temp_user_data.temp_data(user_id)[user_id][0] = 10
-                        bot.send_message(user_id, 'Какая наиболее важная экологическая проблема для Вас?')
-                case 10:
-                    if user_input is not None:
-                        temp_user_data.temp_data(user_id)[user_id][18] = user_input
-                        bot.send_message(user_id, 'Поздравляю, вы успешно прошли тест')
+                        bot.send_message(user_id, 'Произошла ошибка. Введите адрес почты ещё раз')
+                else:
+                    bot.send_message(user_id, 'Это не текст! Попробуйте ещё раз')
+            case 8:
+                if user_input == temp_user_data.temp_data(user_id)[user_id][2]:
+                    bot.send_message(user_id, 'Вы успешно зарегестировались!\nУкажите вашу занятость', reply_markup=buttons.question_btns())
+                else:
+                    bot.send_message(user_id, 'Код не верный, попробуйте еще раз!\n\n'
+                                              'Вы хотите поменять адрес почты?', reply_markup=buttons.email_btns())
+            case 9:
+                if user_input is not None:
+                    temp_user_data.temp_data(user_id)[user_id][13] = user_input
+                    temp_user_data.temp_data(user_id)[user_id][0] = 10
+                    bot.send_message(user_id, 'Какая наиболее важная экологическая проблема для Вас?')
+                else:
+                    bot.send_message(user_id, 'Это не текст! Попробуйте ещё раз')
+            case 10:
+                if user_input is not None:
+                    temp_user_data.temp_data(user_id)[user_id][14] = user_input
+                    db_actions.add_user(user_id, user_nickname, upload_image_to_imgur(temp_user_data.temp_data(user_id)[user_id][6], config.get_config()['imgur_client_id']), temp_user_data.temp_data(user_id)[user_id][4:])
+                    bot.send_message(user_id, 'Поздравляю, вы успешно прошли тест')
+                else:
+                    bot.send_message(user_id, 'Это не текст! Попробуйте ещё раз')
 
     @bot.callback_query_handler(func=lambda call: True)
     def callback(call):
         command = call.data
         user_id = call.message.chat.id
         buttons = Bot_inline_btns()
-        if db_actions.user_is_existed(user_id):
-            if command == 'reg':
-                temp_user_data.temp_data(user_id)[user_id][0] = 0
-                bot.send_message(user_id, 'Введите свой никнейм')
-            elif command == 'y':
-                temp_user_data.temp_data(user_id)[user_id][0] = 7
-                bot.send_message(user_id, 'Введите E-mail')
-            elif command == 'n':
-                send_email(user_id)
-                bot.send_message(user_id, 'Введите код подтвержения с вашей почты')
-            elif command[:8] == 'question':
-                messages = ['Работаю по найму', 'Являюсь самозанятым, ИП или основателем ООО (АО)', 'Не работаю, получаю социальные пособия', 'Я пенсионер', 'Я учусь']
-                temp_user_data.temp_data(user_id)[user_id][15] = messages[int(command[8:])]
-                bot.send_message(user_id, 'Установите вашу категорию', reply_markup=buttons.second_question())
-            elif command[:6] == 'vopros':
-                answers = ['Я малоимущий и пассивный пользователь', 'Мне необходимо активное сотрудничество в группе для совместного роста доходов, улучшения уровня жизни, развития бизнеса и благотворительности', 'Я являюсь инвестором, могу приобретать цифровые финансовые активы у других подписчиков и конвертировать для роста прибыли.']
-                temp_user_data.temp_data(user_id)[user_id][16] = answers[int(command[6:])]
-                temp_user_data.temp_data(user_id)[user_id][0] = 9
-                bot.send_message(user_id, 'Какая наиболее важная социальная проблема для вас?')
+        if command == 'reg':
+            temp_user_data.temp_data(user_id)[user_id][0] = 0
+            bot.send_message(user_id, 'Введите свой никнейм')
+        elif command == 'y':
+            temp_user_data.temp_data(user_id)[user_id][0] = 7
+            bot.send_message(user_id, 'Введите E-mail')
+        elif command == 'n':
+            send_email(user_id)
+            bot.send_message(user_id, 'Введите код подтвержения с вашей почты')
+        elif command[:8] == 'question':
+            messages = ['Работаю по найму', 'Являюсь самозанятым, ИП или основателем ООО (АО)', 'Не работаю, получаю социальные пособия', 'Я пенсионер', 'Я учусь']
+            temp_user_data.temp_data(user_id)[user_id][11] = messages[int(command[8:])]
+            bot.send_message(user_id, 'Установите вашу категорию', reply_markup=buttons.second_question())
+        elif command[:6] == 'vopros':
+            answers = ['Я малоимущий и пассивный пользователь', 'Мне необходимо активное сотрудничество в группе для совместного роста доходов, улучшения уровня жизни, развития бизнеса и благотворительности', 'Я являюсь инвестором, могу приобретать цифровые финансовые активы у других подписчиков и конвертировать для роста прибыли.']
+            temp_user_data.temp_data(user_id)[user_id][12] = answers[int(command[6:])]
+            temp_user_data.temp_data(user_id)[user_id][0] = 9
+            bot.send_message(user_id, 'Какая наиболее важная социальная проблема для вас?')
 
     bot.polling(none_stop=True)
 
@@ -198,8 +220,8 @@ if '__main__' == __name__:
     os_type = platform.system()
     config = ConfigParser(config_name)
     temp_user_data = TempUserData()
-    db = DB(config.get_config()['db_file_name'], Lock())
-    db_actions = DbAct(db, config)
+    # db = DB(config.get_config()['db_file_name'], Lock())
+    db_actions = DbAct(config)
     if config.get_config()['generate_token']:
         db_actions.generate_tokens()
     bot = telebot.TeleBot(config.get_config()['tg_api'])
